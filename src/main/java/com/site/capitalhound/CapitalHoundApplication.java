3a11d9683bffecac45ca59f6d6a7cf7d15ecd217
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2021-01-30 12:50:38 +0200

    Initial commit

---
+++ src/main/java/com/site/capitalhound/CapitalHoundApplication.java
@@ -0,0 +1,13 @@
+package com.site.capitalhound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalHoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalHoundApplication.class, args);
+	}
+
+}

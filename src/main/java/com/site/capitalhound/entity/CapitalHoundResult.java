82207ec893a90af508188e755ef122420979aec5
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2021-01-30 14:23:35 +0200

    Add CapitalHoundResult

---
+++ src/main/java/com/site/capitalhound/entity/CapitalHoundResult.java
@@ -0,0 +1,19 @@
+package com.site.capitalhound.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CapitalHoundResult {
+    private String region;
+    private String countryName;
+    private String flagUrl;
+    private String capitalName;
+    private Double enteredAddressLongitude;
+    private Double enteredAddressLatitude;
+    private Double associatedCapitalLongitude;
+    private Double associatedCapitalLatitude;
+    private Integer population;
+    private Double area;
+}
